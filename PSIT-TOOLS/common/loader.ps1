d9dab67342d14c3b9e234b49a7a3fb35dbad1935
Vojtech Kasny <vojtech.kasny@gmail.com>
2021-01-28 07:13:21 +0100

    add loader

---
+++ PSIT-TOOLS/common/loader.ps1
@@ -0,0 +1,35 @@
+# Import support module a config file
+try {
+    Import-Module (Join-Path $PSScriptRoot '..\modules\support\support.psm1' -Resolve) -Force -DisableNameChecking -ErrorAction Stop
+    $global:PsItTools = Import-PowerShellDataFile (Join-Path $PSScriptRoot 'config.psd1' -Resolve)
+    'Support module loaded successfully', 'Config file loaded successfully' | Write-PSITDebug
+}
+catch {
+    throw ("Loader halted! {0}" -f $_.exception.message)
+}
+
+# Import all other modules
+$ModulesPath = Join-Path $PSScriptRoot '..\modules' -Resolve
+$PsItTools.ModulePath = $ModulesPath
+Add-PSITModulePath $ModulesPath
+(Get-ChildItem $ModulesPath -Directory -Exclude 'support').Name | Import-PSITModule
+
+# Load help file
+$PsItTools.Help = Import-PowerShellDataFile (Join-Path $PSScriptRoot 'help.psd1')
+
+# Set location to user home
+Set-Location $HOME
+
+# Set customized title
+if ($PsItTools.EnableCustomTitle){
+    [console]::Title = '{0} ({1}) | {2}@{3} | {4}' -f $PsItTools.BaseTitle,$PsItTools.Version,$env:USERNAME,$env:COMPUTERNAME, (Get-Date -f 'dd/MM/yyyy')
+}
+
+# Show MD help
+if ($PsItTools.ShowMDHelp){
+    Write-PSITSeparatorLine
+    Get-Content (Join-Path $PSScriptRoot 'README.MD' -Resolve) | ForEach-Object {
+        Write-PSITInfo ('{0}{1}' -f (' '*10),$_)
+    }
+    Write-PSITSeparatorLine
+}
